f90a10cb6bbcf8ed7c99aca798da380c823a9c77
MD. ZAHIDUL ISLAM <74774199+zahid5656@users.noreply.github.com>
2026-07-16 19:03:19 +0600

    build: stop auto-updating KernelSU submodule

--- build-ksunext.sh
+++ build-ksunext.sh
@@ -45,9 +45,6 @@ fi
 echo -e "${YELLOW}Using clang directory: $CLANG_DIR${NC}"
 export PATH="$CLANG_DIR/bin:$PATH"
 
-##SYNC SUBMODULE
-git submodule init && git submodule update
-
 ##Installing necessary components
 ! sudo apt-get install bc git gnupg flex bison build-essential zip curl zlib1g-dev libc6-dev-i386 x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig libssl-dev ccache cpio axel bc build-essential ccache curl device-tree-compiler pandoc libncurses5-dev lynx lz4 fakeroot xz-utils bc build-essential ccache curl device-tree-compiler pandoc lynx lz4 fakeroot xz-utils
 
